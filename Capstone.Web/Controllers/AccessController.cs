2377068305e78820d68a0c2f575af35e81835f02
Tato Arabidze <stylehopex@gmail.com>
2024-02-29 15:16:49 +0400

    Authentication NotDone

---
+++ Capstone.Web/Controllers/AccessController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using TodoListApp.WebApp.Models;
+
+namespace TodoListApp.WebApp.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if(claimUser.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(VMLogin modelLogin)
+        {
+            if (modelLogin.Email == "user@example.com" && modelLogin.Password == "123")
+            {
+                List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                new Claim("OtherProperties","Example Role")
+
+                };
+
+                ClaimsIdentity claimsidentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = modelLogin.KeepLoggedIn
+
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsidentity), properties);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewData["ValidateMessage"] = "user not found";
+            return View();
+        }
+    }
+}
