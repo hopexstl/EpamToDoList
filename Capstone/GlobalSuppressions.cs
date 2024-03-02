b481ddd0f47432a5d8897a56dfecfa7261e8e352
Tato Arabidze <stylehopex@gmail.com>
2024-03-03 02:55:38 +0400

    GlobablSuppressions

--- Capstone/GlobalSuppressions.cs
+++ Capstone/GlobalSuppressions.cs
@@ -1,8 +1,9 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 using System.Diagnostics.CodeAnalysis;
 
+#pragma warning disable SA1404 // Code analysis suppression should have justification
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "<Pending>")]
+#pragma warning restore SA1404 // Code analysis suppression should have justification
